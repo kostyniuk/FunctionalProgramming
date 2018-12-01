13e0165659c58491ae94058d03f841afa9711fba
Alex Kostyniuk <kostyniuk7@gmail.com>
2018-12-01 20:41:00 +0200

    Added commentaries

--- 2-bind.js
+++ 2-bind.js
@@ -1,7 +1,13 @@
+// A function that require two arguments: base of logarithm and a number x
+// Returns logarithm with specified base and number
 const log = (base, n) => Math.log(base) / Math.log(n);
 
-const ln = log.bind(Math.E);
-const lg = log.bind(10);
+// USAGE OF BIND
+// We transform our log function into another in which the first argument is already defined and we can't change it
+// New function will require only one argument because another one is already applied
+const ln = log.bind(null, Math.E);
+const lg = log.bind(null, 10);
 
+// Results
 console.log(`lg(5) = ${lg(5)}`);
 console.log(`ln(5) = ${ln(5)}`);
