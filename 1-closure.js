ca438ed7c83685e2d49b6d751bc2e2d18b8ab956
Alex Kostyniuk <kostyniuk7@gmail.com>
2018-12-01 17:52:23 +0200

    First example added

---
+++ 1-closure.js
@@ -0,0 +1,9 @@
+const log = (base, n) => Math.log(n) / Math.log(base);
+
+const createLog = base => n => log(base, n);
+
+const lg = createLog(10);
+const ln = createLog(Math.E);
+
+console.log(lg(5));
+console.log(ln(10));
