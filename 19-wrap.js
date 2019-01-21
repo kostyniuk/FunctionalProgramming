77e4aad2f05495098aeab74f0c58222b639348f7
Alex Kostyniuk <kostyniuk7@gmail.com>
2019-01-21 12:09:27 +0200

    Commentaries updated

--- 19-wrap.js
+++ 19-wrap.js
@@ -1,4 +1,12 @@
-/* Function wrap that wraps the function, wrapper is lambda that get argumnets from the call and return final result. I created 3 methods for wrapper function. Method cancel off the function and return wrapper. Method timer has parametr msec(miliseconds) after this time expires our function being canceled. Method limit has parametr quantity that show how much times function could be executed before function becomes canceled. Every method returns wrapper so we can use chaining. 
+/* Function wrap that wraps the function, wrapper is lambda
+   that get argumnets from the call and return final result. 
+   I created 3 methods for wrapper function. 
+   Method cancel off the function and return wrapper. 
+   Method timer has parametr msec(miliseconds) after this 
+   time expires our function being canceled. 
+   Method limit has parametr quantity that show how much times 
+   function could be executed before function becomes canceled. 
+   Every method returns wrapper so we can use chaining. 
 */
 const wrap = fn => {
   let limit = 0;
